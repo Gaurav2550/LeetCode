1fb58b97f7dbe5e71886af7c4e7ce05f80f0d45d
Gaurav Thombare <thombareg216@gmail.com>
2026-09-13 07:21:38 +0530

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- string/longest-common-prefix.java
+++ string/longest-common-prefix.java
@@ -1,5 +1,21 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        
+       int minLength = strs[0].length();
+        for(String s : strs){
+            if(s.length() < minLength){
+                minLength = s.length();
+            }
+
+        }
+
+
+        for(int i = 0; i<minLength; i++){
+            for(int j = 1; j<strs.length; j++){
+                if(strs[0].charAt(i) != strs[j].charAt(i)){
+                   return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0].substring(0, minLength);
     }
 }
