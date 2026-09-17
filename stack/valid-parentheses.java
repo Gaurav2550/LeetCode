e7eb19a394cbe0ab90072b53188ed97c0b6a0db5
Gaurav Thombare <thombareg216@gmail.com>
2026-09-17 07:10:51 +0530

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

---
+++ stack/valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();   
+
+        for(char ch : s.toCharArray()){
+            if(ch =='(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            } else{
+                 if(stack.isEmpty()){
+                    return false;
+                 }
+
+                 char top =  stack.pop();
+
+                if( (ch ==')' && ch !='(') ||
+                    (ch ==']' && ch !='[') ||
+                    (ch =='}' && ch !='{') 
+                ){
+
+                    return false;
+
+                }
+            
+            }
+
+
+        }
+
+        return stack.isEmpty();
+    }
+}
